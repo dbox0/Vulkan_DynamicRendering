b92277b93fa5e5629790fe691beefbb41a546f1f
Georgi Dimov <georgi.dimov96@gmail.com>
2026-09-01 12:01:17 +0200

    Create Swapchain and check if color format is supported, create Depth Buffer Image and ImageView

--- src/application.cpp
+++ src/application.cpp
@@ -149,10 +149,171 @@ bool Application::initializeVulkan() {
         showError("Could not create the logical GPU device");
         return false;
     }
+
+    // Use the VMA (Vulkan Memory Allocator) library to avoid PAIN
+    if (!initializeVMA()) {
+        showError("Unable to create Vulkan Memory Allocator");
+        return false;
+    }
+
+    // Swapchain
+    if (!createSwapchain(width,height)) {
+        showError("Unable to create swapchain");
+        return false;
+    }
 }
 
-// TODO: 25:30 createDeivce
+bool Application::createSwapchain(uint32_t width, uint32_t height) {
+    // track swapchain size seperate from window size
 
+    swapchainWidth = width;
+    swapchainHeight = height;
+
+    // ensure we request an appropriate number of images
+    VkSurfaceCapabilitiesKHR surfaceCaps{};
+    if (vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice,surface,&surfaceCaps) != VK_SUCCESS) {
+        showError("Failed to get surface capabilities");
+        return false;
+    }
+
+    uint32_t requestedImageCount = std::max(2u,surfaceCaps.minImageCount);
+    if (surfaceCaps.maxImageCount > 0) {
+        requestedImageCount = std::min(requestedImageCount,surfaceCaps.maxImageCount);
+    }
+
+    VkSwapchainCreateInfoKHR swapchainCreateInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = surface,
+        .minImageCount = requestedImageCount,
+        .imageFormat = swapchainFormat,
+        .imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR,
+        .imageExtent = {.width =  swapchainWidth,.height = swapchainHeight},
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .preTransform = surfaceCaps.currentTransform, // tell os how to orient the image - most cases identity matrix
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = VK_PRESENT_MODE_FIFO_KHR // First in First Out - standard guarantees this. Works well with vsync
+        //.presentMode = VK_PRESENT_MODE_MAILBOX_KHR
+        //.presentMode = VK_PRESENT_MODE_IMMEDIATE_KHR //vsync off mode
+    };
+
+    if (vkCreateSwapchainKHR(device,&swapchainCreateInfo,nullptr,&swapchain) != VK_SUCCESS) {
+        showError("Error creating swapchain");
+        return false;
+    }
+
+    // ask for the swapchain images
+    uint32_t imageCount = 0;
+    vkGetSwapchainImagesKHR(device,swapchain,&imageCount,nullptr);
+    swapchainImages.resize(imageCount);
+    vkGetSwapchainImagesKHR(device,swapchain,&imageCount,swapchainImages.data());
+    swapchainImageViews.resize(imageCount);
+
+    // now create swapchain image views
+    for (size_t i = 0; i < swapchainImages.size(); ++i) {
+        VkImageViewCreateInfo imgViewInfo
+        {
+          .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+            .image = swapchainImages[i],
+            .viewType = VK_IMAGE_VIEW_TYPE_2D,
+            .format = swapchainFormat,
+            .subresourceRange
+            {
+              .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+              .baseMipLevel = 0,
+              .levelCount = 1,
+              .baseArrayLayer = 0,
+              .layerCount = 1
+            }
+        };
+        if (vkCreateImageView(device,&imgViewInfo,nullptr,&swapchainImageViews[i]) != VK_SUCCESS) {
+            showError("Error creating swapchain image view");
+            return false;
+        }
+    }
+
+    // Semaphores signaling render completion and that the image is ready for display
+
+    // Create a semaphore corresponding to each swapchain image
+    renderCompleteSemaphores.resize(swapchainImages.size());
+    for (VkSemaphore &semaphore : renderCompleteSemaphores) {
+        VkSemaphoreCreateInfo semInfo{.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO};
+
+        if (vkCreateSemaphore(device,&semInfo,nullptr,&semaphore) != VK_SUCCESS) {
+            showError("Error creating the ''render-complete'' semaphore");
+            return false;
+        }
+    }
+
+    //create depth-Buffer
+    VkImageCreateInfo depthCreateInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = depthFormat,  // Set at a diffent place
+        .extent {.width = swapchainWidth,.height = swapchainHeight,.depth = 1},
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .tiling = VK_IMAGE_TILING_OPTIMAL, // Memory arangement. TILING_OPTIMAL is a memory arrangement optimal for smapling
+        .usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, // Image can be used during depth or stencil operations
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED
+    };
+
+    // Allocate memory for the image using VMA
+    VmaAllocationCreateInfo allocInfo
+    {
+      .flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    if (vmaCreateImage(vmaAllocator,&depthCreateInfo,&allocInfo,&depthImage,&depthImageAllocation,nullptr) != VK_SUCCESS) {}
+    {
+        showError("Error creating depth buffer image");
+        return false;
+    }
+
+    //Create a view into the image memory that will let us use it as a depth attachment layer
+    VkImageViewCreateInfo depthImgViewInfo
+    {
+      .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image = depthImage,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = depthFormat, // defined in application.h
+        .subresourceRange = {.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,.levelCount = 1,.layerCount = 1}
+    };
+    if (vkCreateImageView(device,&depthImgViewInfo,nullptr,&depthImageView) != VK_SUCCESS) {
+        showError("Error creating depth image view");
+        return false;
+    }
+    return true;
+}
+
+bool Application::initializeVMA() {
+
+    VmaVulkanFunctions vmaFuncInfo{};
+
+    // Provide VMA some info about our setup
+    VmaAllocatorCreateInfo vmaAllocInfo
+    {
+        // We intend to use the buffer device address features of modern vulkan
+        // This will allow us to access vram directly using shaders
+        .flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+        .physicalDevice = physicalDevice,
+        .device = device,
+        .pVulkanFunctions = &vmaFuncInfo,
+        .instance = vulkanInstance,
+        .vulkanApiVersion = VulkanVersion
+    };
+
+    // import directly from volk
+    vmaImportVulkanFunctionsFromVolk(&vmaAllocInfo,&vmaFuncInfo);
+
+    if (vmaCreateAllocator(&vmaAllocInfo,&vmaAllocator) != VK_SUCCESS) {
+        return false;
+    }
+    return true;
+}
 // VkDevice is created via a VkPhysicalDevice
 bool Application::createDevice(VkPhysicalDevice phyiscalDevice) {
     // query the supported features
@@ -293,6 +454,27 @@ VkPhysicalDevice Application::findPhysicalDevice() {
             }
         }
     }
+
+    // Ensure the selected physical device and surface combination can support the swapchain color format
+    uint32_t formatCount = 0;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, nullptr);
+    std::vector<VkSurfaceFormatKHR> surfaceFormats(formatCount);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount,surfaceFormats.data());
+
+    bool formatSupported = false;
+    for (const VkSurfaceFormatKHR &surfFormat : surfaceFormats) {
+        if (surfFormat.format == swapchainFormat) {
+            formatSupported = true;
+            break;
+        }
+    }
+    if (!formatSupported) {
+        showError("Requested swapchain format is not supported by the surface");
+        return nullptr;
+    }
+
+    // If GPU and surface combo cant support the format - check which physical device is being chosen
+    // and review the vector of supported formats that vulkan returns
     return physicalDevice;
 }
 bool Application::createSurface() {
@@ -315,6 +497,20 @@ void Application::shutdown() {
     }
     volkFinalize();
 
+    // VMA
+    if (vmaAllocator) {
+        vmaDestroyAllocator(vmaAllocator);
+    }
+
+    // cleanup Vulkan
+    if (surface) {
+        vkDestroySurfaceKHR(vulkanInstance, surface, nullptr);
+    }
+    if (device) {
+        vkDestroyDevice(device, nullptr);
+    }
+
+    // SDL
     if (window) {
         SDL_DestroyWindow(window);
     }
