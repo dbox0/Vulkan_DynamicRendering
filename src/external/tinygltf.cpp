b3a0ce6d404a7f5d3a6e4e80264db47d34870328
Georgi Dimov <georgi.dimov96@gmail.com>
2026-09-01 18:10:06 +0200

    Add tinygltf and stbimg

---
+++ src/external/tinygltf.cpp
@@ -0,0 +1,7 @@
+#define TINYGLTF3_IMPLEMENTATION
+#define TINYGLTF3_ENABLE_FS
+#define TINYGLTF3_ENABLE_STB_IMAGE
+#include "tiny_gltf_v3.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
